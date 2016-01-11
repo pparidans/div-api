828ad090084bfa9c3d5ed03a6ab12882202b7ae1
Pierre Paridans <pierre@paridans.org>
2016-01-11 11:20:08 +0100

    very simple version without worker

--- check_plate.js
+++ check_plate.js
@@ -1,42 +1,40 @@
 var webpage = require("webpage");
+var system = require("system");
 var url = "http://www.mobilit.fgov.be/WebdivPub_FR/wmvpstv1_fr";
 var loadingCheckerInterval = 100;
 var loadingCheckerCount = 100;
+var plate = system.args[1];
+var page = webpage.create();
+page.clearCookies();
 
-module.exports = function(data, done, worker) {
-  var plate = data.plate;
-  var page = webpage.create();
-  page.clearCookies();
-
-  page.onLoadFinished = function(status) {
-    var loadingCheckerId = setInterval(function(){
-      loadingCheckerCount--;
-      if(loadingCheckerCount === 0) {
-        clearInterval(loadingCheckerId);
-        done(new Error("NoResponseReceived"));
-      }
-      page.switchToFrame("AppWindow");
-      var message = page.evaluate(function() {
-        return document.getElementById('Readonly1').value;
-      });
-      console.log("message", typeof message, message)
-      if(message.trim() !== "") {
-        clearInterval(loadingCheckerId);
-        done(null, { plate: plate, message: message });
-      }
-    }, loadingCheckerInterval);
-  };
-
-  page.open(url, function(status) {
-    if(status !== "success") {
-      done(new Error("CannotLoadPage"), { url: url });
-    } else {
-      page.switchToFrame("AppWindow");
-      // console.log(page)
-      page.evaluate(function(plate) {
-        document.getElementById("Writable2").value = plate;
-        document.getElementById("btnOphalen").click();
-      }, plate);
+page.onLoadFinished = function(status) {
+  var loadingCheckerId = setInterval(function(){
+    loadingCheckerCount--;
+    if(loadingCheckerCount === 0) {
+      clearInterval(loadingCheckerId);
+      console.log(JSON.stringify({ error: "NoResponseReceived" }));
+      phantom.exit(1);
     }
-  });
+    page.switchToFrame("AppWindow");
+    var message = page.evaluate(function() {
+      return document.getElementById('Readonly1').value;
+    });
+    if(typeof message === 'string' && message.trim() !== "") {
+      clearInterval(loadingCheckerId);
+      console.log(JSON.stringify({ plate: plate, message: message }));
+      phantom.exit(0);
+    }
+  }, loadingCheckerInterval);
 };
+
+page.open(url, function(status) {
+  if(status !== "success") {
+    console.log(JSON.stringify({ error: "Error: CannotLoadPage " + url }));
+  } else {
+    page.switchToFrame("AppWindow");
+    page.evaluate(function(plate) {
+      document.getElementById("Writable2").value = plate;
+      document.getElementById("btnOphalen").click();
+    }, plate);
+  }
+});
--- server.js
+++ server.js
@@ -1,52 +1,24 @@
-var express = require("express");
-var morgan = require("morgan");
-var Pool = require("phantomjs-pool").Pool;
+var express = require('express');
+var morgan = require('morgan');
+
+var path = require('path')
+var childProcess = require('child_process')
+var phantomjs = require('phantomjs')
+var binPath = phantomjs.path
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(morgan("combined"));
 
-var platesQueue = [];
-
 app.get('/api/v3/check', function(req, res) {
   var plate = req.query.plate;
-  platesQueue.push({
-    plate: plate,
-    onSuccess: function(plateStatus) {
-      res.send(plateStatus);
-    },
-    onError: function(error) {
-      res.status(500).send({ error: "Error", message: error.message });
-    }
+
+  var childArgs = [ path.join(__dirname, 'check_plate.js'), plate ];
+
+  childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+    res.send(JSON.parse(stdout.toString('utf8')));
   });
 });
-
-var onWorkerReady = function (job, worker, index) {
-  console.log(worker, index)
-  setInterval(function() {
-    if(platesQueue.length !== 0) {
-      var plateEnquiry = platesQueue.pop();
-      var plate = plateEnquiry.plate;
-      console.log("Processing...", plate);
-      job({ plate: plate }, function(err, data) {
-        console.log("DONE", err, data);
-        if(err === null) {
-          plateEnquiry.onSuccess(data);
-        } else {
-          plateEnquiry.onError(err);
-        }
-      });
-    }
-  }, 100);
-};
-
-var pool = new Pool({
-  numWorkers: 1,
-  jobCallback: onWorkerReady,
-  workerFile: __dirname + "/check_plate.js"
-});
-
-pool.start();
 
 var server = app.listen(app.get('port'), function() {
   var host = server.address().address;
